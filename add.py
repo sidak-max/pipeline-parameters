3fc743a7a6ba4fd26cea5a430322e49bda1ef825
sidak-max <sidaksingh051@gmail.com>
2026-08-27 11:06:15 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,6 +1,14 @@
-# Define two numbers
-number_one = 5
-number_two = 10
+import sys
+
+# Define two numbers by pulling from command-line parameters
+# sys.argv[0] is the script name, sys.argv[1] is NUM1, sys.argv[2] is NUM2
+try:
+    number_one = int(sys.argv[1])
+    number_two = int(sys.argv[2])
+except IndexError:
+    # Fallback default values if no arguments are passed
+    number_one = 5
+    number_two = 10
 
 # Calculate the sum
 result = number_one + number_two
