e71b09e4cc43f899618db70d35d0122555796fb0
j v sanjay guptha <sanjayguptha2706@gmail.com>
2026-08-27 10:34:25 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,9 @@
+# Define two numbers
+number_one = 5
+number_two = 10
+
+# Calculate the sum
+result = number_one + number_two
+
+# Display the result
+print(f"The sum of {number_one} and {number_two} is {result}")
